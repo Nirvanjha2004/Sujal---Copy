18eb72c0265d9846f9701ddb2c843324769e175d
Nirvanjha2004 <nirvanjha2004@gmail.com>
2025-10-16 12:40:10 +0530

    Made some code structure changes

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,10 @@
+// Re-export all hooks for easy importing
+export { default as useFavorites } from './useFavorites';
+export { default as useProperties } from './useProperties';
+export { default as useAuth } from './useAuth';
+export { default as useDashboard } from './useDashboard';
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useDebounce } from './useDebounce';
+export { default as usePagination } from './usePagination';
+export { default as useFileUpload } from './useFileUpload';
+export { default as useVisits } from './useVisits';
---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,6 @@
+// Re-export all utilities
+export * from './formatters';
+export * from './validators';
+export * from './helpers';
+export * from './constants';
+export { default as tokenUtils } from './tokenUtils';

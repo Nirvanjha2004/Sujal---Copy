d35164e60ae212739548269d2847a455c2e54302
Nirvanjha2004 <nirvanjha2004@gmail.com>
2025-10-18 13:51:37 +0530

    fixed the errros.. related to backend

--- src/shared/types/auth/index.ts
+++
@@ -1,6 +0,0 @@
-// Shared Auth Types - Accessible by both frontend and backend
-export * from './user';
-export * from './tokens';
-export * from './requests';
-export * from './responses';
-export * from './auth-state';
